e4f0aec4fa21b9da124bf384465033dd3b1aec15
Daniel Ricci <thedanny09@icloud.com>
2019-10-10 12:50:02 -0400

    Another test

--- Pong/main.cpp
+++ Pong/main.cpp
@@ -10,7 +10,6 @@ and may not be redistributed without written permission.*/
 #include <string>
 #include <cmath>
 
-
 //Screen dimension constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
