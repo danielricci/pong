725ed15fb321209eeef3e5d24d2cce76b822285f
Daniel Ricci <thedanny09@icloud.com>
2020-01-09 08:23:24 -0500

    Code cleanup for main

--- Pong/main.cpp
+++ Pong/main.cpp
@@ -1,7 +1,5 @@
 #include "Game/GameWindow.hpp"
 
 int main(int argc, char** argv) {
-    GameWindow* game = new GameWindow("Pong", 640, 480);
-    game->show();
-    delete game;
+    GameWindow("Pong", 640, 480).show();
 }
